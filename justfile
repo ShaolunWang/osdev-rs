e06b49b4e5208a286dd4077f50008eea83861020
ShaolunWang <wang.shaolun@gmail.com>
2025-09-24 20:09:39 +0800

    basic printer

--- justfile
+++ justfile
@@ -1,4 +1,4 @@
 build:
-	cargo build 
+	cargo build && cargo bootimage
 qemu:
 	qemu-system-x86_64 -drive format=raw,file=target/x86_64-osdev/debug/bootimage-osdev.bin
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
 #![no_std]
 #![no_main]
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    use core::fmt::Write;
+
+    vga_buffer::WRITER.lock().write_str("hello world").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", number: {}", 42).unwrap();
     loop {}
 }
