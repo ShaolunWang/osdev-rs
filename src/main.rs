0f5b51b8321a4b446a02f34b764a19624af8a15d
ShaolunWang <wang.shaolun@gmail.com>
2025-09-24 23:48:07 +0800

    feat: print macro

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,12 @@ mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-
-    vga_buffer::WRITER.lock().write_str("hello world").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", number: {}", 42).unwrap();
+    println!("Hello World{}", "!");
     loop {}
 }
